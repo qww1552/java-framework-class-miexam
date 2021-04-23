2cf61711d7732d34a32739e8a978c6661b47af7a
qww1552 <qww1552>
2021-04-23 16:29:13 +0900

    5번 DaoFactory to Spring

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,9 +1,14 @@
-public class DaoFactory {
-    public UserDao getUserDao() {
-        return new UserDao(getConnectionMaker());
-    }
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-    public ConnectionMaker getConnectionMaker() {
+@Configuration
+public class DaoFactory {
+    @Bean
+    public UserDao userDao() {
+        return new UserDao(connectionMaker());
+    }
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new JejuConnectionMaker();
     }
 }
--- src/test/java/UserDaoTests.java
+++ src/test/java/UserDaoTests.java
@@ -1,4 +1,7 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,12 +13,18 @@ public class UserDaoTests {
     String name = "hulk";
     String password = "1234";
 
+    static UserDao userDao;
+
+    @BeforeAll
+    public static void setup() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
+
+
     @Test
     public void testGet() throws SQLException, ClassNotFoundException {
         Integer id = 1;
-
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
 
         User user = userDao.get(id);
 
@@ -29,9 +38,6 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
 
         userDao.insert(user);
 
